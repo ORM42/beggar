f6045c6011f46fa51732bfc4371a4421bcbbcbbe
ORM42 <oleksandr.rymartsov@gmail.com>
2020-08-28 19:59:12 +0300

    scratch

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+      id="app"
+      class="container-fluid"
+  >
+    <Beggar head="Money for honey"/>
+  </div>
+</template>
+
+<script>
+import Beggar from './components/Beggar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Beggar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  text-align: center;
+}
+</style>
---
+++ src/components/Sponsor.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <input
+          type="text"
+          class="form-control"
+          placeholder={{placeholder}}
+          :value="value"
+          v-on=""
+      >
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+    title: String,
+    placeholder: String,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
